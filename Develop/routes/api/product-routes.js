71ca128a7167172d1117bfc634bd49811510ccc9
Alex Canez <el.winy.36@gmail.com>
2022-06-18 12:14:14 -0600

    need to create bulk tags

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -38,7 +38,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -47,30 +47,45 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     },
   */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+  const newProduct = req.body
+  if (newProduct.product_name && newProduct.price && newProduct.stock && newProduct.category_id) {
+    Product.create(newProduct)
+      .then((product) => {
+        // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+        if (req.body.tagIds.length) {
+          const productTagIdArr = req.body.tagIds.map((tag_id) => {
+            return {
+              product_id: product.id,
+              tag_id,
+            };
+          });
+          return ProductTag.bulkCreate(productTagIdArr);
+        }
+        if (!req.body.tagIds) {
+        // if no product tags, just respond
+        res.status(200).json(product);
+       } else {
+         return product;
+       }
+      })
+      .then((product) => {
+        // bulk create of tags 
+        // using req.body.tagIds and product.id
+        // ProductTag.something....
+        res.status(200).json(product);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  } else {
+    res.status(400).json('Missing parameters')
+  }
 });
 
+
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -111,8 +126,11 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const
+  }
 });
 
 module.exports = router;
